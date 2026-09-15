6312f27534fdae206f1bf828780ad5c216f1d7eb
anujjraghuwanshi-ctrl <anujjraghuwanshi@icloud.com>
2026-09-16 03:44:32 +0530

    Fix syntax error in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content:content: ["./index.html", "./*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
